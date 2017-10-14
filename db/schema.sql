9dcd4b4faddac8048ebdccd205fdb1d81d79b4b3
Loyd Charles <loydcharles3@gmail.com>
2017-10-13 23:08:43 -0400

    add files

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+### Schema
+DROP DATABASE IF EXISTS i7pxswt3ocpe6z52;
+CREATE DATABASE i7pxswt3ocpe6z52;
+
+/*
+CREATE TABLE burgers
+(
+	id 			int 		 NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured 	boolean		 DEFAULT  FALSE,
+	date 		timestamp,
+	PRIMARY KEY (id)
+);
+*/
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var port = process.env.PORT || 8080;
+
+var app = express();
+
+var db = require("./models");
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync().then(function() {
+	app.listen(port, function (){ 
+		console.log("Listening on Port: " + port);
+	});
+});	
+
+
+//"production": {
+	//    "username": "root",
+	//    "password": "root",
+	//    "database": "database_production",
+	//    "host": "127.0.0.1",
+//		"dialect": "mysql"
+//	  }
+	
